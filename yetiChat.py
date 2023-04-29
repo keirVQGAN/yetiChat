54c5e1f5e8777e77286f61f169d943573ab717f6
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-04-30 00:38:16 +0100

    Update yetiChat.py

--- yetiChat.py
+++ yetiChat.py
@@ -1,9 +1,109 @@
+import os
+import time
+import warnings
+import shutil
+from google.colab import drive
+from IPython.display import clear_output
+import requests
+import json
 from typing import List, Union, Optional, Dict
-import os, json, openai, datetime, time
 from pathlib import Path
 from rich import print, panel as rich_panel, text as rich_text, console as rich_console
 
 console = rich_console.Console()
+
+def image(title, animal, api_key, prompt, width=512, height=512, samples=2, mask_image=None,
+          prompt_strength=None, num_inference_steps=30, guidance_scale=7, enhance_prompt='no', seed=None,
+          webhook=None, track_id=None):
+    """
+    This function generates an image based on the given prompt, and other parameters.
+    """
+
+    def clean_filename(text):
+        """
+        This function cleans up the given text and returns a cleaned up version.
+        """
+        result = text.replace(' ', '_')
+        return result
+
+    negative = '/content/drive/MyDrive/mani/in/txt/negative_prompts.txt'
+    with open(negative, 'r') as file:
+        negative_prompt = file.read()
+
+    headers = {'Content-Type': 'application/json'}
+    output_dir = f'/content/drive/MyDrive/mani/out/manifestos/{title}'
+    url = 'https://stablediffusionapi.com/api/v3/text2img'
+    data = {
+        "key": api_key,
+        "prompt": prompt,
+        "negative_prompt": negative_prompt,
+        "width": str(width),
+        "height": str(height),
+        "samples": str(samples),
+        "num_inference_steps": str(num_inference_steps),
+        "guidance_scale": guidance_scale,
+        "enhance_prompt": enhance_prompt,
+        "seed": seed,
+        "webhook": webhook,
+        "track_id": track_id
+    }
+
+    if mask_image:
+        data["mask_image"] = mask_image
+
+    if prompt_strength:
+        data["prompt_strength"] = prompt_strength
+
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    return response
+
+
+def setup():
+    """
+    This function sets up the environment, including mounting the Google Drive, removing the sample_data
+    directory, and installing required packages.
+    """
+    start_time = time.time()
+    warnings.filterwarnings('ignore')
+
+    paths = {
+        'drive': '/content/drive',
+        'sample_data': '/content/sample_data',
+        'yetiChat': '/content/yetiChat',
+        'PATH': '/content/drive/MyDrive/yetiChat',
+        'OUT': '/content/drive/MyDrive/yetiChat/out',
+        'IN': '/content/drive/MyDrive/yetiChat/in'
+    }
+
+    PATH = paths['PATH']
+    OUT = paths['OUT']
+    IN = paths['IN']
+
+    if not os.path.ismount(paths['drive']):
+        drive.mount(paths['drive'])
+        clear_output()
+
+    if os.path.isdir(paths['sample_data']):
+        shutil.rmtree(paths['sample_data'])
+
+    if not os.path.isdir(paths['yetiChat']):
+        !git clone https://github.com/keirVQGAN/yetiChat {paths['yetiChat']} &> /dev/null
+        shutil.copy(f'{paths["yetiChat"]}/yetiChat.py', '/content')
+        !pip install -r {paths["yetiChat"]}/requirements.txt &> /dev/null
+        clear_output()
+
+    import yetiChat as yc, openai
+    yc.title('yetiChat 0.0.1', "m" if not os.path.isdir(paths['yetiChat']) else "c")
+
+    if not os.path.isdir(paths['yetiChat']):
+        yc.title('Mounted GDrive', "c")
+        installed_packages = ','.join(open(f'{paths["yetiChat"]}/requirements.txt', 'r').readlines()).strip().replace('\\n', ', ')
+        yc.title(f"Installed ~ {installed_packages}", "y")
+        yc.timeTaken(start_time)
+
+    with open(os.path.join(PATH, 'ini/key.txt'), "r") as f:
+        openai.api_key = f.read().strip("\n")
+
 
 def chatAPI(user: str, system: str, gpt_model: str = "gpt-3.5-turbo", name: Optional[str] = None, temperature: float = 1, top_p: float = 1, n: int = 1, stream: bool = False, stop: Optional[Union[str, List[str]]] = None, max_tokens: Optional[int] = None, presence_penalty: float = 0, frequency_penalty: float = 0, logit_bias: Optional[Dict[int, float]] = None) -> Dict:
     messages = [
@@ -23,19 +123,19 @@ def chatAPI(user: str, system: str, gpt_model: str = "gpt-3.5-turbo", name: Opti
         "presence_penalty": presence_penalty,
         "frequency_penalty": frequency_penalty
     }
-    
+
     if logit_bias is not None:
         api_call_args["logit_bias"] = logit_bias
 
     completion = openai.ChatCompletion.create(**api_call_args)
-    
     gpt_reply = completion.choices[0].message.content.strip()
     title(gpt_reply)
     return completion.to_dict()
 
+
 def saveJSON(file_path: str, data: Dict, mode: str = 'w') -> None:
     file_path = Path(file_path)
-    
+
     if file_path.exists():
         with file_path.open('r') as f:
             existing_data = json.load(f)
@@ -46,17 +146,21 @@ def saveJSON(file_path: str, data: Dict, mode: str = 'w') -> None:
         existing_data.append(data)
         with file_path.open('w') as f:
             json.dump(existing_data, f, indent=4)
-            
+
+
 def heading(title: Optional[str] = None) -> None:
     console.print(rich_console.Rule(title, style="bright_white"))
+
 
 def title(title: str, colour: str = "c") -> None:
     title_text = rich_text.Text(title, style="white")
     colour = {"c": "cyan", "m": "magenta", "y": "yellow"}.get(colour, colour)
     console.print(rich_panel.Panel(title_text, expand=False, style=f"{colour} on black"))
 
+
 def timeTaken(start_time: float) -> None:
     time_taken_float = "%s seconds" % (time.time() - start_time)
     time_taken_split = str(time_taken_float).split('.')
     time_taken_short = time_taken_split[0] + time_taken_split[1][:0]
     title(f'Complete: {time_taken_short} Seconds', "white")
+
